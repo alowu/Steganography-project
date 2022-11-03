7cc88f6b26424192178024513dec0bebc3e8f3e5
Ilia Belousov <ilya.bela@yandex.ru>
2022-11-03 09:29:20 +0300

    refactor

--- core/test.py
+++ core/test.py
@@ -4,13 +4,13 @@ import math
 import re
 
 q = 0.3
-q = input("энергия встраиваемого сигнала: ")
-q = float(q)
+#q = input("энергия встраиваемого сигнала: ")
+#q = float(q)
 c = 3
-c = input("размер области: ")
-c = int(c)
+#c = input("размер области: ")
+#c = int(c)
 text = "its finnaly works with any string length"
-text = input("введите текст: ")
+#text = input("введите текст: ")
 
 x_pos = c
 y_pos = c
@@ -19,12 +19,7 @@ num_rep = 1
 #num_rep = int(num_rep)
 
 
-kotiki = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\kotiki.bmp"
-apple = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\apple.bmp"
-img = cv2.imread(kotiki)
-img_new = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\apple_new.bmp"
-img_w = img.shape[1]
-img_h = img.shape[0]
+
 
 def l(image, x, y):
     return image.item(x, y, 0) * 0.11448 + image.item(x, y, 1) * 0.58662 + image.item(x, y, 2) * 0.29890 #BGR
@@ -49,6 +44,7 @@ def toBinary2(a):
   return m
 
 def cutZero(s):
+    s_new = []
     for i in range(0,8):
         if s[i] == "0":
             continue
@@ -153,7 +149,7 @@ def readByte(image):
         val += str(tmp)
     return val[::-1]
 
-def encode(image, msg):
+def encode(image, msg, img_path):
     global x_pos; x_pos = c
     global y_pos; y_pos = c
     if len(msg) * 8 * num_rep > (int(img_h) / 4 - 1) * (int(img_w) / 4 - 1):
@@ -163,7 +159,8 @@ def encode(image, msg):
     for i in range(0, limit):
         bit = msg[i]
         writeByte(image, bit)
-    cv2.imwrite(kotiki, image)
+    img_path_new = img_path.split('.')[0] + '_encrypted.bmp'
+    cv2.imwrite(img_path_new, image)
 
 def decode(image):
     global x_pos; x_pos = c
@@ -213,31 +210,14 @@ def addLenMsg(msg):
         len_m = tmp + msg
         return len_m
 
-#len_m = addLenMsg("")
-#print(len_m)
-#enc = toBinary(len_m)
-#print(enc)
-#print(toString(enc))
-#print(len(enc))
-#fullMsg = []
-#ind = 0
-#for j in range(0, len(enc)):
-#    byte = enc[j]
-#    for i in reversed(range(0, 8)):
-#        fullMsg.insert(ind, (byte >> i) & 1)
-#        ind += 1
-        #print(bit)
-#print(fullMsg)
-#print(len(fullMsg))
-
-#s = "00110000"
-#s_new = cutZero(s)
-#print(s_new)
-#print(toString2([int(s_new)]))
-
-
+kotiki = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\kotiki.bmp"
+apple = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\apple.bmp"
+img = cv2.imread(kotiki)
+img_new = "C:\\Users\\User\\Documents\\Python\\Steganography-project\\resources\\apple_new.bmp"
+img_w = img.shape[1]
+img_h = img.shape[0]
 
 text = addLenMsg(text)
 text = toBinary2(text)
-encode(img, text)
+encode(img, text, kotiki)
 decode(img)
