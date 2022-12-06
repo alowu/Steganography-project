d4e28e598e4daf1f00fd612aa42c77a6df7513a7
hiccuoboi <denis.shulimovv@gmail.com>
2022-12-06 12:29:39 +0300

    Add information about app

--- main_window.py
+++ main_window.py
@@ -1,6 +1,8 @@
+# coding: utf-8
+
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QLabel, QTextEdit, QTextBrowser
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QLabel, QTextEdit, QTextBrowser, QMessageBox
 from PyQt5.QtGui import QPixmap
 from PIL import ImageQt
 from core.main import main
@@ -12,11 +14,10 @@ fnamel = ''    #глобальная переменная для хранени�
 class MyApp(QMainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
-        self.setWindowTitle('Steganography-project')
         self.window_width, self.window_height = 800, 700
         self.setFixedSize(self.window_width, self.window_height)
 
-        uic.loadUi('image.ui', self)
+        uic.loadUi('project.ui', self)
 
         self.LeftOpenFileButton = self.findChild(QPushButton, 'LeftOpenFileButton')
         self.RightOpenFileButton = self.findChild(QPushButton, 'RightOpenFileButton')
@@ -31,12 +32,15 @@ class MyApp(QMainWindow):
         self.textDecryptionKey = self.findChild(QTextEdit, 'textDecryptionKey')
         self.textKeyBrowser = self.findChild(QTextBrowser, 'textKeyBrowser')
         self.textDecryptionMessageBrowser = self.findChild(QTextBrowser, 'textDecryptionMessageBrowser')
+        self.AboutAppButton = self.findChild(QPushButton, 'AboutAppButton')
+
 
         self.LeftOpenFileButton.clicked.connect(self.open_left_image)
         self.RightOpenFileButton.clicked.connect(self.open_right_image)
         self.SaveFileButton.clicked.connect(self.save_image)
         self.EncryptButton.clicked.connect(self.clicker_encrypt_button)
         self.DecryptButton.clicked.connect(self.clicker_decrypt_button)
+        self.AboutAppButton.clicked.connect(self.show_dialog)
 
         self.show()
 
@@ -83,6 +87,17 @@ class MyApp(QMainWindow):
         DecryptionMessage = main('', Key, '', fnamer, flag)
         print(DecryptionMessage)
         self.textDecryptionMessageBrowser.setText(DecryptionMessage)
+
+    def show_dialog(self):
+        MsgBox = QMessageBox()
+        MsgBox.setIcon(QMessageBox.Information)
+        file = open('README.txt',  'r', encoding='utf-8')
+        AboutText = file.read()
+        file.close()
+        MsgBox.setText(AboutText)
+        MsgBox.setWindowTitle("About App")
+        MsgBox.setStandardButtons(QMessageBox.Ok)
+        retval = MsgBox.exec_()
 
 
 if __name__ == '__main__':
