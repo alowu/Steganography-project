716119b41dfc0b3f7796149a40b3b4f4179cb315
hiccuoboi <denis.shulimovv@gmail.com>
2022-12-06 04:15:36 +0300

    Encryption and decryption working

--- core/main.py
+++ core/main.py
@@ -1,14 +1,14 @@
 import base64
-import decrypt
-import encrypt
+import core.decrypt
+import core.encrypt
 
 
 def texttoimg(text, path, key, q=0.5):
-    img = encrypt.cv2.imread(path)
+    img = core.encrypt.cv2.imread(path)
     img_w = img.shape[1]
     img_h = img.shape[0]
 
-    amount_bits, text = encrypt.tobinary(text)
+    amount_bits, text = core.encrypt.tobinary(text)
 
     key = str(len(text)) + "@" + str(amount_bits) + "@" + key
 
@@ -16,15 +16,10 @@ def texttoimg(text, path, key, q=0.5):
     base64_bytes = base64.b64encode(key_b)
     key_base64 = base64_bytes.decode('ascii')
 
-    print(f'''use this key to decode:
-    --->   {key_base64}   <---''')
 
-    points_x, points_y = encrypt.createlists(amount_bits, img_h, img_w, key_base64)
-    new = encrypt.encode(img, text, path, points_x, points_y, q)
-    if new:
-        print(f'Text is saved in image. Path to it is:'
-              f'{new}')
-    input("Press any key to exit")
+    points_x, points_y = core.encrypt.createlists(amount_bits, img_h, img_w, key_base64)
+    new = core.encrypt.encode(img, text, path, points_x, points_y, q)
+    return new, key_base64
 
 
 def imgtotext(path, key_base64):
@@ -34,44 +29,24 @@ def imgtotext(path, key_base64):
 
     amount_chars, amount_bits, key_decode = key.split('@')
 
-    img_decode = encrypt.cv2.imread(path)
+    img_decode = core.encrypt.cv2.imread(path)
     img_w_decode = img_decode.shape[1]
     img_h_decode = img_decode.shape[0]
-    points_x_decode, points_y_decode = encrypt.createlists(int(amount_bits), img_h_decode, img_w_decode, key_base64)
-    result = decrypt.decode(img_decode, points_x_decode, points_y_decode, int(amount_chars))
-    print(f'Decrypted text is:'
-          f'{result}')
-    input("Press any key to exit")
+    points_x_decode, points_y_decode = core.encrypt.createlists(int(amount_bits), img_h_decode, img_w_decode, key_base64)
+    result = core.decrypt.decode(img_decode, points_x_decode, points_y_decode, int(amount_chars))
+    return result
 
 
-def main():
-    choose = int(input('CHOOSE OPTION\n'
-                       '1 for encrypt text\n'
-                       '2 for decrypt text\n'))
+def main(t, k, q, pi, choose):
 
     if choose == 1:
-        t = k = pi = ''
-        while t == '':
-            t = input("Enter text to encrypt: ")
-        while k == '':
-            k = input("Enter key for encryption: ")
-        while pi == '':
-            pi = input("Copy path to image: ")
-            pi = pi.replace('\\', '\\\\')
-        q = input("Enter energy of encryption in range 0.05 to 1 (or click enter, default 0.5): ")
         if q:
-            texttoimg(t, pi, k, float(q))
+            return texttoimg(t, pi, k, float(q))
         else:
-            texttoimg(t, pi, k)
+            return texttoimg(t, pi, k)
 
     elif choose == 2:
-        pi = k = ''
-        while k == '':
-            k = input("Enter key for decryption: ")
-        while pi == '':
-            pi = input("Copy path to image: ")
-            pi = pi.replace('\\', '\\\\')
-        imgtotext(pi, k)
+        return imgtotext(pi, k)
 
 
 if __name__ == "__main__":
--- main_window.py
+++ main_window.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QLabel, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QLabel, QTextEdit, QTextBrowser
 from PyQt5.QtGui import QPixmap
 from PIL import ImageQt
+from core.main import main
 
+fnamer = ''    #глобальная переменная для хранения пути к правому изображению
+fnamel = ''    #глобальная переменная для хранения пути к левому изображению
 
 
 class MyApp(QMainWindow):
@@ -15,42 +18,69 @@ class MyApp(QMainWindow):
 
         uic.loadUi('image.ui', self)
 
-        self.OpenFileButton = self.findChild(QPushButton, 'OpenFileButton')
+        self.LeftOpenFileButton = self.findChild(QPushButton, 'LeftOpenFileButton')
+        self.RightOpenFileButton = self.findChild(QPushButton, 'RightOpenFileButton')
         self.SaveFileButton = self.findChild(QPushButton, 'SaveFileButton')
         self.EncryptButton = self.findChild(QPushButton, 'EncryptButton')
         self.DecryptButton = self.findChild(QPushButton, 'DecryptButton')
-        self.EEButton = self.findChild(QPushButton, 'EEButton')
-        self.KEButton = self.findChild(QPushButton, 'KEButton')
         self.labelLeft = self.findChild(QLabel, 'labelLeft')
         self.labelRight = self.findChild(QLabel, 'labelRight')
         self.textToEncrypt = self.findChild(QTextEdit, 'textToEncrypt')
-        self.textKeyForEncryption = self.findChild(QTextEdit, 'textKeyForEncryption')
+        self.textEncryptionKey = self.findChild(QTextEdit, 'textEncryptionKey')
         self.textEnergy = self.findChild(QTextEdit, 'textEnergy')
-        self.textKeyForDecryption = self.findChild(QTextEdit, 'textKeyForDecryption')
-        self.textDecrypt = self.findChild(QTextEdit, 'textDecrypt')
+        self.textDecryptionKey = self.findChild(QTextEdit, 'textDecryptionKey')
+        self.textKeyBrowser = self.findChild(QTextBrowser, 'textKeyBrowser')
+        self.textDecryptionMessageBrowser = self.findChild(QTextBrowser, 'textDecryptionMessageBrowser')
 
-        self.OpenFileButton.clicked.connect(self.open_image)
+        self.LeftOpenFileButton.clicked.connect(self.open_left_image)
+        self.RightOpenFileButton.clicked.connect(self.open_right_image)
         self.SaveFileButton.clicked.connect(self.save_image)
         self.EncryptButton.clicked.connect(self.clicker_encrypt_button)
+        self.DecryptButton.clicked.connect(self.clicker_decrypt_button)
 
         self.show()
 
-    def open_image(self):
-        iname = QFileDialog.getOpenFileName(self, 'Open Image', 'D:\\Steganography-project\\resources', 'Image (*.bmp)')
-        self.pixmapLeft = QPixmap(iname[0])
+    def open_left_image(self):
+        global fnamel
+        fnamel = QFileDialog.getOpenFileName(self, 'Open Image', 'D:\\Steganography-project\\resources', 'Image (*.bmp)')[0]    #функция getOpenFileName() возвращает список из двух значений: путь к файлу и расширение; поэтому мы берём только перую переменную (путь к файлу) из возвращаемого списка
+        self.pixmapLeft = QPixmap(fnamel)
         self.labelLeft.setPixmap(self.pixmapLeft)
+
+    def open_right_image(self):
+        global fnamer
+        fnamer = QFileDialog.getOpenFileName(self, 'Open Image', 'D:\\Steganography-project\\resources', 'Image (*.bmp)')[0]    #функция getOpenFileName() возвращает список из двух значений: путь к файлу и расширение; поэтому мы берём только перую переменную (путь к файлу) из возвращаемого списка
+        self.pixmapRight = QPixmap(fnamer)
+        self.labelRight.setPixmap(self.pixmapRight)
 
     def save_image(self):
         image = ImageQt.fromqpixmap(self.labelRight.pixmap())
         image.save('test.jpg')
 
     def clicker_encrypt_button(self):
-        text = self.textToEncrypt.toPlainText()
-        with open('text.txt', 'w') as f:
-            f.write(text)
-
         flag = 1
+        Message = self.textToEncrypt.toPlainText()
+        Key = self.textEncryptionKey.toPlainText()
+        Energy = self.textEnergy.toPlainText()
 
+        path_and_key = main(Message, Key, Energy, fnamel, flag)
+        global fnamer
+        fnamer = path_and_key[0]
+        DecryptionKey = path_and_key[1]
+
+        self.pixmapRight = QPixmap(fnamer)
+        self.labelRight.setPixmap(self.pixmapRight)
+
+        self.textKeyBrowser.setText(DecryptionKey)
+
+    def clicker_decrypt_button(self):
+        flag = 2
+        Key = self.textDecryptionKey.toPlainText()
+        global fnamer
+        print(fnamer, Key)
+
+        DecryptionMessage = main('', Key, '', fnamer, flag)
+        print(DecryptionMessage)
+        self.textDecryptionMessageBrowser.setText(DecryptionMessage)
 
 
 if __name__ == '__main__':
